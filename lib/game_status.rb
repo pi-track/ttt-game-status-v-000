dd50e3395b0e6027a405dc6fd062303ec6a9a7b5
Patrick Eells <patrick.eells@gmail.com>
2018-01-05 14:16:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,16 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0,1,2],  # Top row
+    [3,4,5],  # Middle row
+    [6,7,8],  # Bottom row
+    [0,3,6],  # 1st column
+    [1,4,7],  # 2nd column
+    [3,5,8],  # 3rd column
+    [0,4,8],  # diagonal down
+    [2,4,6]   # diagonal up
+]
+
+def won(board)
+end
